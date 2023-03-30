7dcdf32f62d76b0ae725ff6d045cf462f085a92d
saad1379 <sheikh.saad.riaz@gmail.com>
2023-03-31 00:44:59 +0500

    this is first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as mobilenet from "@tensorflow-models/mobilenet";
 
 function App() {
+  const [model, setModel] = useState(null);
+  const [image, setImage] = useState(null);
+  const [predictions, setPredictions] = useState([]);
+  const imgRef = useRef(null);
+
+  const loadModel = async () => {
+    const mobilenetModel = await mobilenet.load();
+    setModel(mobilenetModel);
+  };
+
+  const classifyImage = async () => {
+    const imgElement = imgRef.current;
+    const tfimg = tf.browser.fromPixels(imgElement);
+    const resizedImg = tf.image.resizeBilinear(tfimg, [224, 224]);
+    const batchedImg = resizedImg.expandDims(0);
+    const predictions = await model.classify(batchedImg);
+    setPredictions(predictions);
+  };
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleImageLoad = () => {
+    classifyImage();
+  };
+
+  if (!model) {
+    loadModel();
+    return <div>Loading model...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      {image && <img src={image} onLoad={handleImageLoad} ref={imgRef} />}
+      {predictions.map((p, i) => (
+        <div key={i}>{`${p.className}: ${p.probability.toFixed(2)}`}</div>
+      ))}
     </div>
   );
 }
